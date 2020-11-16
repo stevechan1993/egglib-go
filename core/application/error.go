43cc9f0e8930f600803e41b8c791b58ff44c4e94
SteveChan <steve.d.chan@qq.com>
2020-11-16 16:45:39 +0800

    feat:add ThrowErrorMsg

--- core/application/error.go
+++ core/application/error.go
@@ -40,6 +40,13 @@ func (serviceError *ServiceError) Error() string {
 	return serviceError.Message
 }
 
+func ThrowErrorMsg(attachMessages ...string) error {
+	return &ServiceError{
+			Code:    501 + baseCode*baseCodeMultiple,
+			Message: strings.Join(append([]string{""}, attachMessages...), ""),
+		}
+}
+
 func ThrowError(serviceErrorNo int, attachMessages ...string) error {
 	switch serviceErrorNo {
 	case INTERNAL_SERVER_ERROR:
